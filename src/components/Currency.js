fb0606ea0bc655dd983aee5e23f8fedf254b7dce
Destiny Frost <destinyrfrost@gmail.com>
2023-09-13 15:32:53 -0400

    Synced currency to alert box
    Signed-off-by: Destiny Frost <destinyrfrost@gmail.com>

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -15,7 +15,7 @@ const Currency = () => {
 
     return (
 		<div className='alert alert-secondary'>
-        <label style={{marginLeft: '1rem' , backgroundColor:'#33FF49', color:'white'}} >Currency
+        <label style={{marginLeft: '1rem' , backgroundColor:'#FFD700', color:'black'}} >Currency
         <select name='hover_color'id="currency" onChange={(event)=>changeCurrency(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'#33FF49', color:'white'}}>
         <option style={{color:'black'}} value="£">£ Pound</option>
         <option style={{color:'black'}} value="$">$ Dollar</option>
