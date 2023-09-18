2590ec00297989b168aa3fc7aa405c35c01a6e7e
Destiny Frost <destinyrfrost@gmail.com>
2023-09-18 11:40:29 -0400

    Currency color updated
    Signed-off-by: Destiny Frost <destinyrfrost@gmail.com>

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -15,8 +15,8 @@ const Currency = () => {
 
     return (
 		<div className='alert alert-secondary'>
-        <label style={{marginLeft: '1rem' , backgroundColor:'#FFD700', color:'black'}} >Currency
-        <select name='hover_color'id="currency" onChange={(event)=>changeCurrency(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'#33FF49', color:'white'}}>
+        <label style={{marginLeft: '1rem' , color:'black'}} >Currency
+        <select name='hover_color'id="currency" onChange={(event)=>changeCurrency(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'#33FF49', color:'black'}}>
         <option style={{color:'black'}} value="£">£ Pound</option>
         <option style={{color:'black'}} value="$">$ Dollar</option>
         <option style={{color:'black'}} value="€">€ Euro</option>
